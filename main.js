29875cf24446a944eaa8645633bbedb0dec8de0d
Milos Petrovic <milos.petrovic09.95@gmail.com>
2024-09-14 19:05:44 +0200

    animation added

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 import './style.css'
+var previousVersionDiv = document.getElementById("previousVersion");
+var animationStarted = false;
 
+window.addEventListener("scroll", function() {
+    var scrollTop = window.scrollY || document.documentElement.scrollTop;
+    var triggerOffset = previousVersionDiv.offsetTop - window.innerHeight + 100;
+
+    // Aktiviraj animaciju kada korisnik dođe do div-a
+    if (scrollTop > triggerOffset && !animationStarted) {
+        previousVersionDiv.style.transform = "translateY(0)";
+        animationStarted = true;
+    }
+});
